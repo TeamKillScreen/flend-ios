592861b5d0ba7f6c32d038d70bdad3014cb1b1ad
Alan Gorton <dev.code1up@googlemail.com>
2012-10-28 02:38:18 +0000

    Initial Commit

---
+++ flend/FCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FCAppDelegate.h
+//  flend
+//
+//  Created by Alan Gorton on 28/10/2012.
+//  Copyright (c) 2012 Alan Gorton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ flend/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  flend
+//
+//  Created by Alan Gorton on 28/10/2012.
+//  Copyright (c) 2012 Alan Gorton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FCAppDelegate class]));
+    }
+}
